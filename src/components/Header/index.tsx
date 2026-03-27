751491bbe1e6fe2c51b0983401a7de79820adb93
diego-marques-miranda <diegomm.pu@gmail.com>
2026-03-27 19:40:40 -0300

    add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,6 @@
+export const Header = () => (
+  <header className="py-3 shadow-lg text-center bg-dark text-warning">
+    <h1 className="h3 fw-bold m-0 tracking-tighter text-uppercase">Sky Control Hub</h1>
+    <small className="text-white opacity-50">SISTEMA DE MONITORAMENTO DE TRÁFEGO</small>
+  </header>
+);
